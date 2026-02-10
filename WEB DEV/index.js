1a81b7cf75eab3c4c17a72e36d920d23dbe2068b
Krishna Kumar <krishnajais7091@gmail.com>
2026-02-10 07:08:10 +0530

    first commit

---
+++ WEB DEV/index.js
@@ -0,0 +1,16 @@
+// function genrator
+function rectangle(name,age){
+    let student = {
+        studentName:name,
+        studentAge:age,
+        Course:`B.tech`
+    };
+    return student;
+}
+
+console.log(`Just Cheking`);
+
+console.log(rectangle(`Krishna`,20));
+console.log(rectangle(`Aman`,21));
+console.log(rectangle(`Mayank`,21));
+
