68415de92e7db47552c343f6d206bee919026886
Krishna Kumar <krishnajais7091@gmail.com>
2026-02-10 07:21:05 +0530

    update

--- WEB DEV/index.js
+++ WEB DEV/index.js
@@ -1,16 +1,31 @@
 // function genrator
-function rectangle(name,age){
-    let student = {
-        studentName:name,
-        studentAge:age,
-        Course:`B.tech`
-    };
-    return student;
+// function rectangle(name,age){
+//     let student = {
+//         studentName:name,
+//         studentAge:age,
+//         Course:`B.tech`
+//     };
+//     return student;
+// }
+
+// console.log(`Just Cheking`);
+
+// console.log(rectangle(`Krishna`,20));
+// console.log(rectangle(`Aman`,21));
+// console.log(rectangle(`Mayank`,21));
+
+// primitive pass by copy
+// let a = 10;
+// function inc(a){
+//     a++;
+// }
+// inc(a);
+// console.log(a);
+
+// object pass by copy address
+let b ={ value:10};
+function inc(a){
+    b.value++;
 }
-
-console.log(`Just Cheking`);
-
-console.log(rectangle(`Krishna`,20));
-console.log(rectangle(`Aman`,21));
-console.log(rectangle(`Mayank`,21));
-
+inc(b);
+console.log(b);
